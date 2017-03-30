439ec490d8b16b55ed70b5c306957b3209933538
jack01310131 <jack01310131@gmail.com>
2017-03-30 19:07:10 +0800

    update

--- 3_23_hw/3_23_hw.php
+++ 3_23_hw/3_23_hw.php
@@ -1,10 +1,11 @@
 <?php
-	$endtime=date_create("2017-12-31");
+	$endtime=date_create('2017-12-31 0:0:0');
 	$nowtime=date_create(date('Y-m-j'));
 	$mon=date('m');
 	
 	echo "<img src=\"".$mon.".png\"><br>";
-
+	$now=date('Y-m-j H:i:s');
+	$end=date('2017-12-31 0:0:0');
 	echo date('Y年m月j日l H時i分s秒'),"<br>";
 	
 	$diff=date_diff($nowtime,$endtime);
@@ -22,6 +23,13 @@
 	}else{
 		$diffday= 30-date('j');
 	}
-		
+	
 	echo "距離世界末日還剩：",$diffyear,"年",$diffmon,"月",$diffday,"日<br>";
+
+	$diffsecond=floor((strtotime($end)-strtotime($now)));
+	echo "距離世界末日還剩：",(int)($diffsecond/86400),"天";
+	echo (int)(($diffsecond%86400)/3600),"時";
+	echo (int)((($diffsecond%86400)%3600)/60),"分";
+	echo (int)(((($diffsecond%86400)%3600)%60)),"秒";
+
 ?>
